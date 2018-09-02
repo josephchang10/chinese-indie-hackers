9c5555f5719d4345e6166ba9d396682cb12b18a7
Joseph Chang <zhangkuan523@gmail.com>
2018-09-02 16:41:05 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,391 @@
+var authors = [
+    {
+        "name": "waylybaye",
+        "artistId": "1119881923",
+        "weibo": "https:\/\/weibo.com\/u\/1673953377"
+    },
+    {
+        "name": "\u949f\u9896",
+        "artistId": "928924733",
+        "weibo": "https:\/\/weibo.com\/u\/1765732340"
+    },
+    {
+        "name": "im61",
+        "artistId": "784494589",
+        "weibo": "https:\/\/weibo.com\/u\/1734549517"
+    },
+    {
+        "name": "KyXu",
+        "artistId": "988271193",
+        "weibo": "https:\/\/weibo.com\/u\/3559103571"
+    },
+    {
+        "name": "simpleapples",
+        "artistId": "549139622",
+        "github": "https:\/\/github.com\/simpleapples"
+    },
+    {
+        "name": "GhostSKB",
+        "artistId": "1134384858",
+        "github": "https:\/\/github.com\/dingmingxin\/GhostSKB"
+    },
+    {
+        "name": "Gao Deng",
+        "artistId": "590504524",
+        "github": "https:\/\/github.com\/gaodeng"
+    },
+    {
+        "name": "Soledad",
+        "artistId": "1088715054",
+        "github": "https:\/\/github.com\/caiyue1993",
+        "weibo": "https:\/\/weibo.com\/caiyue233\/"
+    },
+    {
+        "name": "NextStack",
+        "artistId": "1343060592",
+        "github": "https:\/\/github.com\/safe-dog",
+        "website": "https:\/\/blog.nextstack.xyz\/"
+    },
+    {
+        "name": "Jason",
+        "artistId": "1056935451",
+        "weibo": "https:\/\/weibo.com\/u\/2018707532",
+        "website": "https:\/\/atjason.com\/"
+    },
+    {
+        "name": "Jinya",
+        "artistId": "1107941617",
+        "weibo": "https:\/\/weibo.com\/934249787",
+        "github": "https:\/\/github.com\/JinyaX"
+    },
+    {
+        "name": "Akring",
+        "artistId": "1073829177",
+        "github": "https:\/\/github.com\/akring"
+    },
+    {
+        "name": "JonyFang",
+        "artistId": "1208128428",
+        "weibo": "http:\/\/weibo.com\/u\/3034766044",
+        "github": "https:\/\/github.com\/JonyFang"
+    },
+    {
+        "name": "haozes",
+        "artistId": "1071202242",
+        "github": "https:\/\/github.com\/haozes"
+    },
+    {
+        "name": "\u6ed1\u6ed1\u9e21",
+        "artistId": "1052013882",
+        "weibo": "https:\/\/weibo.com\/u\/1634553604",
+        "github": "https:\/\/github.com\/songkuixi"
+    },
+    {
+        "name": "MORECATS",
+        "artistId": "1016315440",
+        "github": "https:\/\/github.com\/MORECATS"
+    },
+    {
+        "name": "jiangboLee",
+        "artistId": "1209480380",
+        "github": "https:\/\/github.com\/jiangboLee"
+    },
+    {
+        "name": "biqinglin",
+        "artistId": "1228179246",
+        "github": "https:\/\/github.com\/biqinglin"
+    },
+    {
+        "name": "lfb-cd",
+        "artistId": "1198077659",
+        "github": "https:\/\/github.com\/lfb-cd"
+    },
+    {
+        "name": "Larry",
+        "artistId": "1223916907",
+        "github": "https:\/\/weibo.com\/u\/2267155115",
+        "website": "http:\/\/maliquankai.com\/",
+        "weibo": "https:\/\/weibo.com\/u\/2267155115"
+    },
+    {
+        "name": "\u53f6\u5b64\u57ce",
+        "artistId": "1251368572",
+        "weibo": "https:\/\/weibo.com\/u\/1438670852"
+    },
+    {
+        "name": "\u56fe\u62c9\u9f0e",
+        "artistId": "724472957",
+        "weibo": "https:\/\/weibo.com\/u\/1846569133"
+    },
+    {
+        "name": "\u5468\u6977\u96ef",
+        "artistId": "951710534",
+        "weibo": "https:\/\/weibo.com\/u\/1758053453"
+    },
+    {
+        "name": "coderyi",
+        "artistId": "1003756405",
+        "github": "https:\/\/github.com\/coderyi"
+    },
+    {
+        "name": "Tolecen",
+        "artistId": "826439308",
+        "weibo": "https:\/\/weibo.com\/u\/1860159237",
+        "website": "https:\/\/xinle.co\/"
+    },
+    {
+        "name": "mw2c",
+        "artistId": "1150303936",
+        "github": "https:\/\/github.com\/mw2c"
+    },
+    {
+        "name": "jkpang",
+        "artistId": "1314212519",
+        "weibo": "https:\/\/weibo.com\/u\/5743737098"
+    },
+    {
+        "name": "Haocold",
+        "artistId": "1050633438",
+        "github": "https:\/\/github.com\/xjh093"
+    },
+    {
+        "name": "\u5f20\u5609\u592b",
+        "artistId": "717622923",
+        "github": "https:\/\/github.com\/josephchang10",
+        "weibo": "https:\/\/weibo.com\/2949394297"
+    },
+    {
+        "name": "miliPolo",
+        "artistId": "1106354714",
+        "github": "https:\/\/github.com\/miliPolo"
+    },
+    {
+        "name": "enzo-yang",
+        "artistId": "1273031711",
+        "github": "https:\/\/github.com\/enzo-yang"
+    },
+    {
+        "name": "Chars",
+        "artistId": "1176787144",
+        "github": "https:\/\/github.com\/charsdavy",
+        "website": "http:\/\/chars.tech\/"
+    },
+    {
+        "name": "SpongeBobSun",
+        "artistId": "1183663531",
+        "github": "https:\/\/github.com\/SpongeBobSun\/"
+    },
+    {
+        "name": "luowei",
+        "artistId": "990121194",
+        "github": "https:\/\/github.com\/luowei",
+        "weibo": "https:\/\/weibo.com\/u\/1745746500"
+    },
+    {
+        "name": "HyanCat",
+        "artistId": "1064830289",
+        "github": "https:\/\/github.com\/HyanCat",
+        "website": "https:\/\/hyancat.com\/"
+    },
+    {
+        "name": "fujianjin6471",
+        "artistId": "1113262918"
+    },
+    {
+        "name": "Drinking",
+        "artistId": "1312729971",
+        "github": "https:\/\/github.com\/drinking",
+        "website": "http:\/\/drinking.github.io\/"
+    },
+    {
+        "name": "Arczzir",
+        "artistId": "461272069",
+        "github": "https:\/\/github.com\/arczzir"
+    },
+    {
+        "name": "Airing",
+        "artistId": "1135703344",
+        "github": "https:\/\/github.com\/airingursb",
+        "website": "http:\/\/ursb.me\/"
+    },
+    {
+        "name": "luckytianyiyan",
+        "artistId": "1063716478",
+        "github": "https:\/\/github.com\/luckytianyiyan",
+        "website": "https:\/\/tyy.sh\/"
+    },
+    {
+        "name": "cg200776",
+        "artistId": "1175974944"
+    },
+    {
+        "name": "\u75d5\u8ff9",
+        "artistId": "1180300715",
+        "github": "https:\/\/github.com\/lijy91",
+        "website": "https:\/\/www.jianshu.com\/u\/7f33d5b97f55"
+    },
+    {
+        "name": "\u5f20\u5c0f\u521a\u54df",
+        "artistId": "1158663522",
+        "weibo": "https:\/\/weibo.com\/u\/3127372955"
+    },
+    {
+        "name": "markmiao",
+        "artistId": "1221228241",
+        "github": "https:\/\/github.com\/mxdios",
+        "website": "http:\/\/markmiao.com\/"
+    },
+    {
+        "name": "TenSteps",
+        "artistId": "1140465592",
+        "github": "https:\/\/github.com\/R0uter",
+        "website": "http:\/\/markmiao.com\/",
+        "weibo": "https:\/\/weibo.com\/u\/6332141409"
+    },
+    {
+        "name": "iHTCboy",
+        "artistId": "914453386",
+        "github": "https:\/\/github.com\/iHTCboy",
+        "website": "https:\/\/ihtcboy.com\/"
+    },
+    {
+        "name": "AlvinZhu",
+        "artistId": "600457010",
+        "github": "https:\/\/github.com\/gbammc",
+        "website": "http:\/\/alvinzhu.me\/"
+    },
+    {
+        "name": "BeckTabs",
+        "artistId": "1274519910"
+    },
+    {
+        "name": "\u5c0f\u828b\u5934\u541b",
+        "artistId": "893450818",
+        "github": "https:\/\/github.com\/xinyu198736"
+    },
+    {
+        "name": "xiaobaiso",
+        "artistId": "1342693448",
+        "github": "https:\/\/github.com\/xiaobaiso",
+        "website": "https:\/\/xiaobaiso.github.io\/"
+    },
+    {
+        "name": "emenwin",
+        "artistId": "532181039"
+    },
+    {
+        "name": "Sugar",
+        "artistId": "1165321483",
+        "weibo": "https:\/\/weibo.com\/u\/1740048760"
+    },
+    {
+        "name": "\u7f50\u5b50icodesign",
+        "artistId": "1239860606",
+        "weibo": "https:\/\/weibo.com\/u\/2203521742"
+    },
+    {
+        "name": "HeyJonny",
+        "artistId": "961945907",
+        "weibo": "https:\/\/weibo.com\/u\/2813718033"
+    },
+    {
+        "name": "an00na",
+        "artistId": "347232661",
+        "weibo": "https:\/\/weibo.com\/u\/1676354212"
+    },
+    {
+        "name": "Yachen Liu",
+        "artistId": "921900701"
+    },
+    {
+        "name": "xDEHANG",
+        "artistId": "1250290964",
+        "weibo": "https:\/\/weibo.com\/u\/2051487233"
+    },
+    {
+        "name": "\u674e\u4fcacoder",
+        "artistId": "1028902680",
+        "weibo": "https:\/\/weibo.com\/u\/1714780543"
+    },
+    {
+        "name": "o1xhack",
+        "artistId": "1178446966",
+        "weibo": "https:\/\/weibo.com\/u\/3788755962"
+    },
+    {
+        "name": "Vinni-Wong",
+        "artistId": "1146099285",
+        "weibo": "https:\/\/weibo.com\/u\/1808884742"
+    },
+    {
+        "name": "Polaris\u5450\u5450",
+        "artistId": "1068107051",
+        "weibo": "https:\/\/weibo.com\/u\/2270839712"
+    },
+    {
+        "name": "pa_patrick",
+        "artistId": "1375958274",
+        "weibo": "https:\/\/weibo.com\/u\/1267671563"
+    },
+    {
+        "name": "derekcoder",
+        "artistId": "1153535935",
+        "weibo": "https:\/\/weibo.com\/u\/6155322764"
+    },
+    {
+        "name": "KITTEN-YANG",
+        "artistId": "1260925934",
+        "weibo": "https:\/\/weibo.com\/u\/2854163804"
+    },
+    {
+        "name": "anniApp",
+        "artistId": "1084335468",
+        "weibo": "https:\/\/weibo.com\/u\/5918998246"
+    },
+    {
+        "name": "\u5f20\u5f13",
+        "artistId": "1040146639",
+        "weibo": "https:\/\/weibo.com\/u\/1933262040"
+    },
+    {
+        "name": "\u8bfb\u4e66\u4f7f\u4eba\u5934\u5927",
+        "artistId": "1395700698",
+        "weibo": "https:\/\/weibo.com\/u\/5822241060"
+    },
+    {
+        "name": "Fnoz",
+        "artistId": "1104673141",
+        "weibo": "https:\/\/weibo.com\/u\/1633562383"
+    },
+    {
+        "name": "\u51ac\u74dc",
+        "artistId": "1234066800",
+        "weibo": "https:\/\/weibo.com\/u\/3633493894"
+    },
+    {
+        "name": "\u4f26\u6566iOS\u5c0f\u725b",
+        "artistId": "460419472",
+        "weibo": "https:\/\/weibo.com\/u\/2822867275"
+    },
+    {
+        "name": "\u96f7\u7eaf\u950b2011",
+        "artistId": "961330939",
+        "weibo": "https:\/\/weibo.com\/u\/1914435431"
+    },
+    {
+        "name": "Yves3",
+        "artistId": "1366556066",
+        "weibo": "https:\/\/weibo.com\/u\/1714354562"
+    },
+    {
+        "name": "Yves3",
+        "artistId": "954188947",
+        "weibo": "https:\/\/weibo.com\/u\/2352150534"
+    },
+    {
+        "name": "JinkeyAI",
+        "artistId": "1132845873",
+        "weibo": "https:\/\/weibo.com\/u\/1894983214",
+        "website": "https:\/\/jinkey.ai"
+    }
+]
---
+++ index.js
@@ -0,0 +1,42 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        apps: [],
+        authors: authors
+    },
+
+    created: function() {
+        this.fetchData()
+        console.log(authors)
+    },
+
+    methods: {
+        fetchData: function() {
+            this.authors.forEach(author => {
+                var xhr = new XMLHttpRequest()
+                var self = this
+                xhr.open('GET', "https://itunes.apple.com/lookup?id=" + author.artistId + "&entity=software&lang=zh-CN&country=CN")
+                xhr.onload = function() {
+                    let results = JSON.parse(xhr.responseText).results
+                    results.shift()
+                    self.apps = self.apps.concat(results)
+                    self.apps.sort(function(a, b){
+                        var dateA = new Date(a.currentVersionReleaseDate)
+                        var dateB = new Date(b.currentVersionReleaseDate)
+                        if (dateA < dateB) {
+                            return 1
+                        }
+                        if (dateA > dateB) {
+                            return -1
+                        }
+                        return 0
+                    })
+                }
+                xhr.send()
+            });
+        },
+        timeAgo: function(dateString) {
+            return moment(dateString).fromNow()
+        }
+    }
+});
