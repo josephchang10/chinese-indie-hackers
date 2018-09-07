fe341c81486c326c1ef6947bab725cca812bbf65
Joseph Chang <zhangkuan523@gmail.com>
2018-09-07 07:42:18 +0100

    增加一位作者

--- data.js
+++ data.js
@@ -488,5 +488,10 @@ var authors = [
         "name": "整理猫儿",
         "artistId": "1294350323",
         "weibo": "https://www.weibo.com/hldhi"
+    },
+    {
+        "name": "Robin灬桑",
+        "artistId": "919152304",
+        "weibo": "https://weibo.com/u/2314991674"
     }
 ]
